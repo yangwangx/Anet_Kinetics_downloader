14f71e280d192b57d7a7479fabfa134a67b0d0ea
Yang Wang <yangwangx@gmail.com>
2017-10-25 23:42:38 -0400

    updates

--- fetch_Kinetics_videos.sh
+++ fetch_Kinetics_videos.sh
@@ -6,15 +6,9 @@ TEMP_FILE=$3
 
 if [ -d $VIDEOPATH ]; then
     python run_crosscheck_Kinetics.py $VIDEOPATH $JSON_FILE $TEMP_FILE
-    if [ -f $TEMP_FILE ]; then
-        bash $TEMP_FILE
-    else
-        echo "File $TEMP_FILE does not exists."
-    fi
 else
     echo "Directory does not exists."
     exit 0
 fi
 
-rm $TEMP_FILE
 echo "Have a good day!"
